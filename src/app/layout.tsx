8daef4348aa16c5e628ab13fb5f7c5f1c941fc43
rdidier <renaud.didier@maincare.fr>
2024-01-07 18:43:13 +0100

    refactor: move navbar in the layout layer
     It will be useful when using the other links, because they will
     automatically use the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
+import { Box, Image, Flex, Spacer, Link, Center } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +20,34 @@ export default function RootLayout({
     return (
         <html lang='fr'>
             <body>
-                <Providers>{children}</Providers>
+                <Providers>
+                    <Center bg='tomato' h='100px' color='white'>
+                        <Flex alignItems='center' width='full'>
+                            <Spacer />
+                            <Link as={NextLink} href='/'>
+                                Accueil
+                            </Link>
+                            <Spacer />
+                            <Link as={NextLink} href='/company'>
+                                L&apos;entreprise
+                            </Link>
+                            <Spacer />
+                            <Link as={NextLink} href='/location'>
+                                Nous trouver
+                            </Link>
+                            <Spacer />
+                            <Link as={NextLink} href='/openingTime'>
+                                Horaires d&apos;ouverture
+                            </Link>
+                            <Spacer />
+                            <Link as={NextLink} href='/contact'>
+                                Contacts
+                            </Link>
+                            <Spacer />
+                        </Flex>
+                    </Center>
+                    <Box>{children}</Box>
+                </Providers>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,3 @@
-import NextLink from 'next/link';
-import { Flex, Spacer, Link, Center } from '@chakra-ui/react';
-
 export default function Home() {
-    return (
-        <Center bg='tomato' h='100px' color='white'>
-            <Flex alignItems='center' width='full'>
-                <Spacer />
-                <Link as={NextLink} href='/'>
-                    Accueil
-                </Link>
-                <Spacer />
-                <Link as={NextLink} href='/company'>
-                    L&apos;entreprise
-                </Link>
-                <Spacer />
-                <Link as={NextLink} href='/location'>
-                    Nous trouver
-                </Link>
-                <Spacer />
-                <Link as={NextLink} href='/openingTime'>
-                    Horaires d&apos;ouverture
-                </Link>
-                <Spacer />
-                <Link as={NextLink} href='/contact'>
-                    Contacts
-                </Link>
-                <Spacer />
-            </Flex>
-        </Center>
-    );
+    return <div>Hello world</div>;
 }
