e05122ea2a4a0fd4db5d184a9f44ebc653a411be
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-01-16 21:26:43 +0100

    have the menu bar fixed
     and change the scrollbar style (to be smaller and black)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,38 +18,61 @@ export default function RootLayout({
         <html lang='fr'>
             <body>
                 <Providers>
-                    <Center
-                        fontWeight='600'
-                        fontFamily={'system-ui'}
-                        bg='black'
-                        h='100px'
-                        color='white'
-                    >
-                        <Flex alignItems='center' width='full'>
-                            <Spacer />
-                            <Link as={NextLink} href='/'>
-                                Accueil
-                            </Link>
-                            <Spacer />
-                            <Link as={NextLink} href='/coaching'>
-                                Coaching individuel
-                            </Link>
-                            <Spacer />
-                            <Link as={NextLink} href='/programme'>
-                                Programme mensuel
-                            </Link>
-                            <Spacer />
-                            <Link as={NextLink} href='/presentation'>
-                                Qui suis-je ?
-                            </Link>
-                            <Spacer />
-                            <Link as={NextLink} href='/contact'>
-                                Me contacter
-                            </Link>
-                            <Spacer />
-                        </Flex>
-                    </Center>
-                    <Box>{children}</Box>
+                    <Box height={'100vh'}>
+                        <Center
+                            fontWeight='600'
+                            fontFamily={'system-ui'}
+                            bg='black'
+                            h='100px'
+                            color='white'
+                        >
+                            <Flex alignItems='center' width='full'>
+                                <Spacer />
+                                <Link as={NextLink} href='/'>
+                                    Accueil
+                                </Link>
+                                <Spacer />
+                                <Link as={NextLink} href='/coaching'>
+                                    Coaching individuel
+                                </Link>
+                                <Spacer />
+                                <Link as={NextLink} href='/programme'>
+                                    Programme mensuel
+                                </Link>
+                                <Spacer />
+                                <Link as={NextLink} href='/presentation'>
+                                    Qui suis-je ?
+                                </Link>
+                                <Spacer />
+                                <Link as={NextLink} href='/contact'>
+                                    Me contacter
+                                </Link>
+                                <Spacer />
+                            </Flex>
+                        </Center>
+                        <Box
+                            height={'calc(100% - 100px)'}
+                            overflowY={'auto'}
+                            sx={{
+                                '&::-webkit-scrollbar': {
+                                    width: '16px',
+                                    height: '16px',
+                                },
+                                '&::-webkit-scrollbar-track': {
+                                    background:
+                                        'linear-gradient(90deg,#434343,#434343 1px,#111 0,#111)',
+                                },
+                                '&::-webkit-scrollbar-thumb': {
+                                    background: '#434343',
+                                    'border-radius': '16px',
+                                    'box-shadow':
+                                        'inset 2px 2px 2px hsla(0,0%,100%,.25),inset -2px -2px 2px rgba(0,0,0,.25)',
+                                },
+                            }}
+                        >
+                            {children}
+                        </Box>
+                    </Box>
                 </Providers>
             </body>
         </html>
