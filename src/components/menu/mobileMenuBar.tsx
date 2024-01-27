912bf5c31bfc4fe06b114e19021cc975d3ab41ec
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-01-27 15:18:17 +0100

    have a button in mobile mode to display or hide menu
    menu is hidden by default

--- src/components/menu/mobileMenuBar.tsx
+++ src/components/menu/mobileMenuBar.tsx
@@ -1,24 +1,41 @@
-import { Link, Grid, GridItem } from '@chakra-ui/react';
+import { Link, Grid, GridItem, IconButton, Icon } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useState } from 'react';
 import LinkItem from 'showed/lib/menu/entities/link';
+import { TiThMenu } from 'react-icons/ti';
 
 export default function MobileMenuBar({ links }: { links: LinkItem[] }) {
+    const [isMenuDisplayed, setIsMenuDisplayed] = useState(false);
     return (
-        <Grid
-            templateColumns={'repeat(1, ' + links.length + ')'}
-            fontWeight='600'
-            fontFamily={'system-ui'}
-            bg='black'
-            color='white'
-            padding='15px'
-        >
-            {links.map((linkItem) => (
-                <GridItem key={linkItem.target} h='30px'>
-                    <Link as={NextLink} href={linkItem.target}>
-                        {linkItem.label}
-                    </Link>
-                </GridItem>
-            ))}
-        </Grid>
+        <>
+            <IconButton
+                position='absolute'
+                right='1px'
+                color='white'
+                backgroundColor='black'
+                aria-label='Afficher/Masquer le menu'
+                icon={<Icon as={TiThMenu} />}
+                onClick={() => setIsMenuDisplayed(!isMenuDisplayed)}
+            />
+            <Grid
+                minHeight='40px'
+                templateColumns={'repeat(1, ' + links.length + ')'}
+                fontWeight='600'
+                fontFamily={'system-ui'}
+                bg='black'
+                color='white'
+                padding='15px'
+            >
+                {isMenuDisplayed
+                    ? links.map((linkItem) => (
+                          <GridItem key={linkItem.target} h='30px'>
+                              <Link as={NextLink} href={linkItem.target}>
+                                  {linkItem.label}
+                              </Link>
+                          </GridItem>
+                      ))
+                    : ''}
+            </Grid>
+        </>
     );
 }
