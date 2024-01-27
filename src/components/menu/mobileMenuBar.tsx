67fe71e100e287d38d79d1315685a7383d2b5759
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-01-27 14:54:27 +0100

    have the mobile header to be more spaced

--- src/components/menu/mobileMenuBar.tsx
+++ src/components/menu/mobileMenuBar.tsx
@@ -10,9 +10,10 @@ export default function MobileMenuBar({ links }: { links: LinkItem[] }) {
             fontFamily={'system-ui'}
             bg='black'
             color='white'
+            padding='15px'
         >
             {links.map((linkItem) => (
-                <GridItem key={linkItem.target}>
+                <GridItem key={linkItem.target} h='30px'>
                     <Link as={NextLink} href={linkItem.target}>
                         {linkItem.label}
                     </Link>
