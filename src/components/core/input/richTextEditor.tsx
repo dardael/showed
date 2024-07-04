f136b146e2ffbccb787460e5410e4b690446819e
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-07-04 17:28:31 +0200

    remose useless code

--- src/components/core/input/richTextEditor.tsx
+++ src/components/core/input/richTextEditor.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/css/froala_style.min.css';
-import { useRef } from 'react';
 import 'froala-editor/js/plugins.pkgd.min.js';
 import 'froala-editor/js/languages/fr.js';
 import FroalaEditor from 'react-froala-wysiwyg';
@@ -13,11 +12,9 @@ export default function RichTextEditor({
     initialData: any;
     onChange: (model: string) => void;
 }) {
-    const editorRef = useRef(null);
     return (
         <Box width={'100%'}>
             <FroalaEditor
-                ref={editorRef}
                 model={initialData}
                 onModelChange={onChange}
                 config={{
