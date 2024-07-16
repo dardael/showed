4473a69e12c0bd9baea207e001509cf8887c87b8
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-07-16 23:03:35 +0200

    have the right color for confirmation button
    it was always blue

--- src/components/core/button/confirmationButton.tsx
+++ src/components/core/button/confirmationButton.tsx
@@ -53,7 +53,6 @@ export default function ConfirmationButton({
                             {modal.cancelText}
                         </Button>
                         <Button
-                            colorScheme='blue'
                             onClick={() => {
                                 modal.onConfirm();
                                 onClose();
