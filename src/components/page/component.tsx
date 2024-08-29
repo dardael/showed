c80bb4bbc39a3fbf03858c548123043db31be668
Renaud DIDIER <renaud.didier.tis@gmail.com>
2024-08-29 13:58:00 +0200

    do not have hydratation error with coutdown
    to avoid this, render it only on client side

--- src/components/page/component.tsx
+++ src/components/page/component.tsx
@@ -1,16 +1,19 @@
 import { Component as ComponentModel } from 'showed/lib/page/models/component';
 import RichText from './component/richText';
-import Countdown from './component/countdown';
 import { Box } from '@chakra-ui/react';
 import Header from './component/header';
 import BoldText from './component/boldText';
 import Text from './component/text';
+import dynamic from 'next/dynamic';
 
 export default async function Component({
     component,
 }: {
     component: ComponentModel;
 }) {
+    const Countdown = dynamic(() => import('./component/countdown'), {
+        ssr: false,
+    });
     return (
         <Box padding={'10px'}>
             {component.componentType === 'RICH_TEXT_EDITOR' && (
