5a795278546d4f68b12d11334a8163faa75b73f7
Renaud DIDIER <renaud.didier.tis@gmail.com>
2025-01-07 13:31:16 +0100

    have a summary of invitations

--- src/components/admin/invitations/invitedPeople.tsx
+++ src/components/admin/invitations/invitedPeople.tsx
@@ -1,9 +1,20 @@
 import {
+    Box,
     Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Center,
+    Divider,
+    Heading,
+    Stack,
+    StackDivider,
     Table,
     TableCaption,
     TableContainer,
     Tbody,
+    Td,
+    Text,
     Th,
     Thead,
     Tr,
@@ -24,40 +35,87 @@ export default function InvitatedPeople({
         });
     }, []);
     return (
-        <TableContainer>
-            <Table>
-                <TableCaption>
-                    <Button
-                        onClick={() =>
-                            loadInvitedPeople().then((people) =>
-                                setInvitedPeople(people)
-                            )
-                        }
-                    >
-                        Rafraichir
-                    </Button>
-                </TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>Prénom</Th>
-                        <Th>Nom</Th>
-                        <Th maxW={'150px'}>{'Enfant / Adulte'}</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {invitedPeople.map((person) => (
-                        <Tr key={person._id}>
-                            <Th>{person.name}</Th>
-                            <Th>{person.surname}</Th>
-                            <Th maxW={'150px'}>
-                                {person.lifeStage === LifeStage.ADULT
-                                    ? 'Adulte'
-                                    : 'Enfant'}
-                            </Th>
+        <>
+            <Card>
+                <CardHeader>
+                    <Heading size='md'>Résumé</Heading>
+                </CardHeader>
+
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='4'>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                {"Nombre d'enfant"}
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                {
+                                    invitedPeople.filter(
+                                        (person) =>
+                                            person.lifeStage === LifeStage.CHILD
+                                    ).length
+                                }
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                {"Nombre d'adulte"}
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                {
+                                    invitedPeople.filter(
+                                        (person) =>
+                                            person.lifeStage === LifeStage.ADULT
+                                    ).length
+                                }
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                {'Nombre total'}
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                {invitedPeople.length}
+                            </Text>
+                        </Box>
+                    </Stack>
+                </CardBody>
+            </Card>
+            <Divider padding={'25px'} />
+            <TableContainer>
+                <Table>
+                    <TableCaption>
+                        <Button
+                            onClick={() =>
+                                loadInvitedPeople().then((people) =>
+                                    setInvitedPeople(people)
+                                )
+                            }
+                        >
+                            Rafraichir
+                        </Button>
+                    </TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Prénom</Th>
+                            <Th>Nom</Th>
+                            <Th maxW={'150px'}>{'Enfant / Adulte'}</Th>
                         </Tr>
-                    ))}
-                </Tbody>
-            </Table>
-        </TableContainer>
+                    </Thead>
+                    <Tbody>
+                        {invitedPeople.map((person) => (
+                            <Tr key={person._id}>
+                                <Td>{person.name}</Td>
+                                <Td>{person.surname}</Td>
+                                <Td maxW={'150px'}>
+                                    {person.lifeStage === LifeStage.ADULT
+                                        ? 'Adulte'
+                                        : 'Enfant'}
+                                </Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
