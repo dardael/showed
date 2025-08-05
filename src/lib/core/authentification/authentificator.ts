220a60c8c6c8fe8bcc2a2125500e8f7791ca3aeb
Renaud DIDIER <renaud.didier.tis@gmail.com>
2025-08-05 16:00:24 +0200

    refactor(authentification): have only one method to test if a token is valid

--- src/lib/core/authentification/authentificator.ts
+++ src/lib/core/authentification/authentificator.ts
@@ -1,46 +1,43 @@
 import AuthentificatorInterface from 'showed/lib/core/authentification/service/authentificator';
 import RepositoryInterface from 'showed/lib/core/authentification/repository';
+import { Authentified } from './models/authentified';
 export default class Authentificator implements AuthentificatorInterface {
     private repository: RepositoryInterface;
     constructor(repository: RepositoryInterface) {
         this.repository = repository;
     }
+
     public async isAlreadyAuthentified(token: string): Promise<boolean> {
         const authentified = await this.repository.getAuthentified(token);
-
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-        const isAlreadyAuthentified =
-            authentified !== null && authentified.createdAt
-                ? new Date(authentified.createdAt) >= oneWeekAgo
-                : false;
-        if (!isAlreadyAuthentified && authentified) {
+        const isValid = this.isValidToken(authentified);
+        if (!isValid && authentified) {
             await this.repository.deleteAuthentified(token);
         }
-        return isAlreadyAuthentified;
+        return isValid;
     }
+
     public async saveAuthentification(token: string): Promise<void> {
         const authentified = await this.repository.getAuthentified(token);
-
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-        if (authentified?.createdAt) {
-            const createdDate = new Date(authentified.createdAt);
-
-            if (createdDate < oneWeekAgo) {
-                await this.repository.deleteAuthentified(token);
-                await this.repository.createAuthentified(token);
-            } else {
-                return;
-            }
-        } else {
-            await this.repository.createAuthentified(token);
+        if (this.isValidToken(authentified)) {
+            return;
         }
+        if (authentified) {
+            await this.repository.deleteAuthentified(token);
+        }
+        await this.repository.createAuthentified(token);
     }
+
     public async logout(token: string): Promise<void> {
         await this.repository.deleteAuthentified(token);
         return;
+    }
+
+    private isValidToken(authentified: Authentified | null): boolean {
+        const oneWeekAgo = new Date();
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+        return authentified !== null && authentified.createdAt
+            ? new Date(authentified.createdAt) >= oneWeekAgo
+            : false;
     }
 }
