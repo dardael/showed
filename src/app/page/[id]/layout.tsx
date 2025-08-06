fdbf2b0a3ccfa141040feb88036b51cb4d014396
Renaud DIDIER <renaud.didier.tis@gmail.com>
2025-08-06 15:43:59 +0200

    correct the way layout are used
    console error was logged when navigating on the website. It was because
    layout was imbricated and add serveral times html and body balise. now
    this system is correctly used

--- src/app/page/[id]/layout.tsx
+++ src/app/page/[id]/layout.tsx
@@ -1,22 +1,11 @@
 import 'showed/lib/core/dependencyInjection/container';
-import type { Metadata } from 'next';
 import 'showed/app/globals.css';
-import { Providers } from 'showed/app/providers';
 import MenuBar from 'showed/components/menu/menuBar';
 import FooterBar from 'showed/components/footer/footerBar';
 import { Box, Flex } from '@chakra-ui/react';
 import { getPages } from 'showed/controllers/page/pageController';
 import { getSocialNetworks } from 'showed/controllers/socialNetwork/socialNetworkController';
-import { getTheme } from 'showed/controllers/theme/themeController';
 
-export async function generateMetadata(): Promise<Metadata> {
-    const theme = await getTheme();
-    return {
-        title: theme.title,
-        description: theme.description,
-        icons: { icon: './favicon.ico' },
-    };
-}
 export default async function RootLayout({
     children,
 }: {
@@ -24,41 +13,34 @@ export default async function RootLayout({
 }) {
     const pages = await getPages();
     const socialNetworks = await getSocialNetworks();
-    const theme = await getTheme();
     return (
-        <html lang='fr'>
-            <body>
-                <Providers initialTheme={theme}>
-                    <Flex height={'100vh'} direction='column'>
-                        <Box flex='0 0 fit-content' maxH={'fit-content'}>
-                            <MenuBar pages={pages} />
-                        </Box>
-                        <Flex
-                            direction='column'
-                            flex='1'
-                            overflowX={'hidden'}
-                            overflowY={'auto'}
-                            sx={{
-                                '&::-webkit-scrollbar': {
-                                    width: '16px',
-                                    height: '16px',
-                                },
-                                '&::-webkit-scrollbar-track': {
-                                    background:
-                                        'linear-gradient(90deg,#434343,#434343 1px,#111 0,#111)',
-                                },
-                                '&::-webkit-scrollbar-thumb': {
-                                    background: '#434343',
-                                    borderRadius: '18px',
-                                },
-                            }}
-                        >
-                            <Box flex='1'>{children}</Box>
-                            <FooterBar socialNetworks={socialNetworks} />
-                        </Flex>
-                    </Flex>
-                </Providers>
-            </body>
-        </html>
+        <Flex height={'100vh'} direction='column'>
+            <Box flex='0 0 fit-content' maxH={'fit-content'}>
+                <MenuBar pages={pages} />
+            </Box>
+            <Flex
+                direction='column'
+                flex='1'
+                overflowX={'hidden'}
+                overflowY={'auto'}
+                sx={{
+                    '&::-webkit-scrollbar': {
+                        width: '16px',
+                        height: '16px',
+                    },
+                    '&::-webkit-scrollbar-track': {
+                        background:
+                            'linear-gradient(90deg,#434343,#434343 1px,#111 0,#111)',
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                        background: '#434343',
+                        borderRadius: '18px',
+                    },
+                }}
+            >
+                <Box flex='1'>{children}</Box>
+                <FooterBar socialNetworks={socialNetworks} />
+            </Flex>
+        </Flex>
     );
 }
